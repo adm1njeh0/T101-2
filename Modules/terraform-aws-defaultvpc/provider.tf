137894e9b03c96c7bd4f4c5c8634aaee43f58b50
adm1njeh0 <52900923+adm1njeh0@users.noreply.github.com>
2023-07-29 16:37:43 +0900

    Create provider.tf

---
+++ Modules/terraform-aws-defaultvpc/provider.tf
@@ -0,0 +1,13 @@
+# provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
